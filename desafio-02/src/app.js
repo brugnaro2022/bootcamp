dc51327185c2de5bb0d0027b6a6f6ecf1790a8d7
marcos <marcosrodriguesbrugnaro@gmail.com>
2020-04-08 00:46:16 -0300

    Get and put repository

--- desafio-02/src/app.js
+++ desafio-02/src/app.js
@@ -5,12 +5,13 @@ const { uuid } = require('uuidv4')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+
+// app.use(cors())
 
 const repositories = []
 
 app.get('/repositories', (request, response) => {
-  // TODO
+  return response.json(repositories)
 })
 
 app.post('/repositories', (request, response) => {
@@ -24,7 +25,23 @@ app.post('/repositories', (request, response) => {
 })
 
 app.put('/repositories/:id', (request, response) => {
-  // TODO
+  const { id } = request.params
+  
+  const { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(
+    repository => repository.id === id
+  )
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' })
+  }
+
+  const repository = { id, title, url, techs }
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 })
 
 app.delete('/repositories/:id', (req, res) => {
