ce8659ea1ada07b85a307edc9ebd9627c94bec39
marcos <marcosrodriguesbrugnaro@gmail.com>
2020-04-13 21:44:34 -0300

    List projects

--- conceitos-dev/frontend/src/App.js
+++ conceitos-dev/frontend/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 import Header from './components/Header'
+import api from './services/api'
+import './App.css'
 
 function App () {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
+
+  function handleAppProject () {
+    setProjects([...projects, `Novo Projeto ${Date.now()}`])
+    console.log(projects)
+  }
+
   return (
     <>
-      <Header />
-      <Header />
+      <Header title='Projects' />
+      <ul>
+        {projects.map(project => (
+          <li key={project.id}>{project.title}</li>
+        ))}
+      </ul>
+      <button type='button' onClick={handleAppProject}>
+        Adicionar Projeto
+      </button>
     </>
   )
 }
