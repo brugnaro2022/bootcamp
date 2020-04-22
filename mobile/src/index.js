5716cbd9126f035a2aaafe74b566ec021a171d98
marcos <marcosrodriguesbrugnaro@gmail.com>
2020-04-21 21:19:52 -0300

    Continue mobile project

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,27 +1,46 @@
-import React from 'react'
-import { Text, View, StyleSheet, StatusBar } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import {
+  Text,
+  SafeAreaView,
+  FlatList,
+  StyleSheet,
+  StatusBar
+} from 'react-native'
+
+import api from './services/api'
 
 export default function App () {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
+
   return (
     <>
       <StatusBar barStyle='light-content' backgroundColor='#7159C1' />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     backgroundColor: '#7159C1',
-    flex: 1,
-    justifyContent: 'center'
+    flex: 1
   },
-  title: {
+  project: {
     color: '#FFF',
-    fontSize: 20,
-    fontWeight: 'bold'
+    fontSize: 20
   }
 })
